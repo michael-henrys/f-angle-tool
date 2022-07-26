5ae1dc6edda66dd0d5a13bcc8683b1fb358fd5b6
michael henrys <michael42h@gmail.com>
2022-07-26 15:17:05 +1200

    handle submit of form

--- main.js
+++ main.js
@@ -6,3 +6,18 @@ function calculateRockFallVolume(formData) {
   vol_ln =(6.431*Math.log(angle))+(1.576*Math.log(height))+(0.526*Math.log(horizontalPGA))-37.122
   return Math.exp(vol_ln)*surfaceArea;
 }
+
+const form = document.getElementById('calculatorInput')
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  //get form data
+  const formData = new FormData(e.target)
+  const formDataObj = {};
+  formData.forEach((value, key) => (formDataObj[key] = value))
+  //calculate volume
+  const volume = calculateRockFallVolume(formDataObj)
+  //display volume
+  const volumeOutput = document.getElementById('volumeOutput')
+  volumeOutput.innerHTML = volume.toFixed(0) + ' m<sup>3</sup>'
+})
+
