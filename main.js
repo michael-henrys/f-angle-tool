e629590459e73a6eed2b765ec7b7292b13f349b5
michael henrys <michael42h@gmail.com>
2022-07-29 10:48:29 +1200

    add graphing logic

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const xValues = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10, 10.5, 11, 11.5, 12, 12.5, 13, 13.5, 14, 14.5, 15]
+
+//set up form and add event listener for calculate button
+const form = document.getElementById('calculatorInput')
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  update()
+})
+
+//initially update the volume and graph
+update()
+
 function calculateRockFallVolume(formData) {
   const angle = formData.angle
   const height = formData.height
@@ -7,37 +19,60 @@ function calculateRockFallVolume(formData) {
   return Math.exp(vol_ln)*surfaceArea;
 }
 
-function handleCalculate(e) {
-  e.preventDefault()
+function update() {
   //get form data
-  const formData = {
-    angle: e.target.elements.angle.value,
-    height: e.target.elements.height.value,
-    horizontalPGA: e.target.elements.horizontalPGA.value,
-    surfaceArea: e.target.elements.surfaceArea.value
-  }
+  formData = getFormData()
   //calculate volume
   const volume = calculateRockFallVolume(formData)
   //display volume
   const volumeOutput = document.getElementById('volumeOutput')
   volumeOutput.innerHTML = volume.toFixed(0) + ' m<sup>3</sup>'
+  //update Graph 
+  updateGraph()
 }
 
-const form = document.getElementById('calculatorInput')
-form.addEventListener('submit', handleCalculate)
-
-graph = document.getElementById('graph');
-const data = [{
-	x: [1, 2, 3, 4, 5],
-	y: [1, 2, 4, 8, 16] }]
-
-const layout = { 
-  margin: {t: 10, l: 30, r: 30, pad: 4}
-} 
-
-const config = {
-  displayModeBar: false,
-  responsive: true
+function getFormData(){
+  return {
+    angle: form.elements.angle.value,
+    height: form.elements.height.value,
+    horizontalPGA: form.elements.horizontalPGA.value,
+    surfaceArea: form.elements.surfaceArea.value
+  }
 }
 
-Plotly.newPlot( graph, data, layout, config )
+function updateGraph(){
+  graph = document.getElementById('graph');
+  const yValues = xValues.map(x => calculateRockFallVolume({ ...getFormData(), horizontalPGA: x }))
+  const trace1 = {
+    x: xValues, 
+    y: yValues,
+    mode: 'lines'
+  }
+
+  const trace2 = {
+    x: [form.elements.horizontalPGA.value],
+    y: [calculateRockFallVolume(getFormData())],
+    mode: 'markers',
+  }
+  
+  const data = [trace1, trace2]
+
+  const layout = { 
+    margin: {t: 10, l: 50, r: 30, pad: 4},
+    yaxis: {
+      title: 'Rockfall Volume (m<sup>3</sup>)',
+      rangemode: 'tozero'
+    },
+    xaxis: {
+      title: 'Horizontal PGA (m/s<sup>2</sup>)',
+      rangemode: 'tozero'
+    }
+  } 
+
+  const config = {
+    displayModeBar: false,
+    responsive: true
+  }
+
+  Plotly.newPlot( graph, data, layout, config )
+}
